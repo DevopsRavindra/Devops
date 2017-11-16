9e4ce7d65528e1eb067c036a889a6aba9e5c8416
DevopsRavindra <33695130+DevopsRavindra@users.noreply.github.com>
2017-11-16 18:56:56 +0530

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,18 @@
+class helloworld
+{
+public static void main(String[] args)
+{
+int a,b,c;
+a=30;
+b=50;
+c=10;
+if(a>b && a>c)
+System.out.println("A is the Biggest Values in Three values"+a);
+else
+if(b>a && b>c)
+System.out.println("B is the Biggest Values in Three values"+B);
+else
+if(c>a && c>b)
+System.out.println("B is the Biggest Values in Three values"+B);
+}
+}
